9530b3e4676091edda29f42b71f0994067d39ad3
Gee Law <geelaw@outlook.com>
2022-06-23 15:05:48 -0700

    Implement `Bst32IntegersCommand` and `Bst32StringsCommand`.

--- src/Neat.BibTeX/BstModel/Bst32IntegersCommand.cs
+++ src/Neat.BibTeX/BstModel/Bst32IntegersCommand.cs
@@ -1,5 +1,6 @@
-using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text;
 using Neat.BibTeX.Utils;
 
 using StringT = Neat.Unicode.String32;
@@ -8,33 +9,53 @@ using Utf = Neat.Unicode.Utf;
 namespace Neat.BibTeX.BstModel
 {
   /// <summary>
-  /// Represents a <c>INTEGERS</c> command.
+  /// Represents an <c>INTEGERS { ... }</c> command.
   /// </summary>
+  [DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}", Type = nameof(Bst32IntegersCommand))]
   public sealed class Bst32IntegersCommand : Bst32Command
   {
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebuggerDisplay
+    {
+      get
+      {
+        return Type.GenericToString() + " { ... }";
+      }
+    }
+
     /// <summary>
     /// The command type of a <c>INTEGERS</c> command.
     /// </summary>
     public static readonly StringT CommandType = Utf.String16ToString32Strict("INTEGERS");
 
-    [MethodImpl(Helper.OptimizeInline)]
+    /// <summary>
+    /// The names of integer variables.
+    /// This array should not be <see langword="null"/>, should contain only valid identifiers, and should not contain duplicates.
+    /// </summary>
+    public StringT[] VariableNames;
+
+    [MethodImpl(Helper.JustOptimize)]
     public sealed override string ToString()
     {
-      return Type.ToString();
+      return ToStringHelper(new StringBuilder().Append(Type.GenericToString()).Append('\n'), 2, VariableNames).ToString();
     }
 
     /// <param name="type">Must be <c>INTEGERS</c> (in any casing).</param>
+    /// <param name="variableNames">Must not be <see langword="null"/>, must contain only valid identifiers, and must not contain duplicates.</param>
     [MethodImpl(Helper.OptimizeInline)]
-    public Bst32IntegersCommand(StringT type)
+    public Bst32IntegersCommand(StringT type, StringT[] variableNames)
       : base(type)
     {
+      VariableNames = variableNames;
     }
 
     #region Bst32Command overrides
 
+    [MethodImpl(Helper.JustOptimize)]
     public sealed override bool IsValid()
     {
-      throw new NotImplementedException();
+      return BibBstComparer.Equals(Type, CommandType)
+        && IsNotNullAndContainsOnlyIdentifiersAndContainsNoDuplicates(VariableNames);
     }
 
     [MethodImpl(Helper.OptimizeInline)]
--- src/Neat.BibTeX/BstModel/Bst32StringsCommand.cs
+++ src/Neat.BibTeX/BstModel/Bst32StringsCommand.cs
@@ -1,5 +1,6 @@
-using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Text;
 using Neat.BibTeX.Utils;
 
 using StringT = Neat.Unicode.String32;
@@ -8,33 +9,53 @@ using Utf = Neat.Unicode.Utf;
 namespace Neat.BibTeX.BstModel
 {
   /// <summary>
-  /// Represents a <c>STRINGS</c> command.
+  /// Represents a <c>STRINGS { ... }</c> command.
   /// </summary>
+  [DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}", Type = nameof(Bst32StringsCommand))]
   public sealed class Bst32StringsCommand : Bst32Command
   {
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebuggerDisplay
+    {
+      get
+      {
+        return Type.GenericToString() + " { ... }";
+      }
+    }
+
     /// <summary>
     /// The command type of a <c>STRINGS</c> command.
     /// </summary>
     public static readonly StringT CommandType = Utf.String16ToString32Strict("STRINGS");
 
-    [MethodImpl(Helper.OptimizeInline)]
+    /// <summary>
+    /// The names of string variables.
+    /// This array should not be <see langword="null"/>, should contain only valid identifiers, and should not contain duplicates.
+    /// </summary>
+    public StringT[] VariableNames;
+
+    [MethodImpl(Helper.JustOptimize)]
     public sealed override string ToString()
     {
-      return Type.ToString();
+      return ToStringHelper(new StringBuilder().Append(Type.GenericToString()).Append('\n'), 2, VariableNames).ToString();
     }
 
     /// <param name="type">Must be <c>STRINGS</c> (in any casing).</param>
+    /// <param name="variableNames">Must not be <see langword="null"/>, must contain only valid identifiers, and must not contain duplicates.</param>
     [MethodImpl(Helper.OptimizeInline)]
-    public Bst32StringsCommand(StringT type)
+    public Bst32StringsCommand(StringT type, StringT[] variableNames)
       : base(type)
     {
+      VariableNames = variableNames;
     }
 
     #region Bst32Command overrides
 
+    [MethodImpl(Helper.JustOptimize)]
     public sealed override bool IsValid()
     {
-      throw new NotImplementedException();
+      return BibBstComparer.Equals(Type, CommandType)
+        && IsNotNullAndContainsOnlyIdentifiersAndContainsNoDuplicates(VariableNames);
     }
 
     [MethodImpl(Helper.OptimizeInline)]
