e889d34f1e54cf3889107142a4aa7d0ca7fad984
Gee Law <geelaw@outlook.com>
2022-06-23 15:13:22 -0700

    Implement `Bst32MacroCommand`.

--- src/Neat.BibTeX/BstModel/Bst32MacroCommand.cs
+++ src/Neat.BibTeX/BstModel/Bst32MacroCommand.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Neat.BibTeX.Utils;
 
@@ -8,33 +8,62 @@ using Utf = Neat.Unicode.Utf;
 namespace Neat.BibTeX.BstModel
 {
   /// <summary>
-  /// Represents a <c>MACRO</c> command.
+  /// Represents a <c>MACRO {name} {"value"}</c> command.
   /// </summary>
+  [DebuggerDisplay("{" + nameof(DebuggerDisplay) + ",nq}", Type = nameof(Bst32MacroCommand))]
   public sealed class Bst32MacroCommand : Bst32Command
   {
+    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+    private string DebuggerDisplay
+    {
+      get
+      {
+        return ToString();
+      }
+    }
+
     /// <summary>
     /// The command type of a <c>MACRO</c> command.
     /// </summary>
     public static readonly StringT CommandType = Utf.String16ToString32Strict("MACRO");
 
-    [MethodImpl(Helper.OptimizeInline)]
+    /// <summary>
+    /// The name of the macro.
+    /// This string should be a valid identifier and should be compared by <see cref="BibBstComparer"/>.
+    /// </summary>
+    public StringT Name;
+
+    /// <summary>
+    /// The value of the macro.
+    /// This string should be a valid quote-delimited literal.
+    /// </summary>
+    public StringT Value;
+
+    [MethodImpl(Helper.JustOptimize)]
     public sealed override string ToString()
     {
-      return Type.ToString();
+      return Type.GenericToString() + " {" + Name.GenericToString() + "} {\"" + Value.GenericToString() + "\"}";
     }
 
     /// <param name="type">Must be <c>MACRO</c> (in any casing).</param>
+    /// <param name="name">Must be a valid identifer.</param>
+    /// <param name="value">Must be a valid quote-delimited literal.</param>
     [MethodImpl(Helper.OptimizeInline)]
-    public Bst32MacroCommand(StringT type)
+    public Bst32MacroCommand(StringT type, StringT name, StringT value)
       : base(type)
     {
+      Name = name;
+      Value = value;
     }
 
     #region Bst32Command overrides
 
+    [MethodImpl(Helper.JustOptimize)]
     public sealed override bool IsValid()
     {
-      throw new NotImplementedException();
+      return BibBstComparer.Equals(Type, CommandType)
+        && BibBstChars.IsIdentifier(Name)
+        && BibBstChars.IsQuoteLiteral(Value);
     }
 
     [MethodImpl(Helper.OptimizeInline)]
